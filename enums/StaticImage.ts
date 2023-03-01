d12190baca1028360a07f5b30362ea223781163c
ngonhat352 <ngonhat352@gmail.com>
2023-03-01 13:15:13 -0600

    fix loading placeholder img

--- enums/StaticImage.ts
+++ enums/StaticImage.ts
@@ -2,10 +2,10 @@
  * Static images src enums
  */
 export enum StaticImage {
-    ERROR = 'error.webp',
+    ERROR = '/error.webp',
     ETH = '/eth.png',
     WETH = '/weth.png',
-    SPINNER = '/spinner.gif',
+    SPINNER = '/spinner.webp',
     SKILLET = '/skillet.png',
     METAMASK = '/metamaskLogo.gif'
 }
